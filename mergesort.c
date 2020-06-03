54b8bee67b1bbc6af256e982b38f1c25546c7020
Akhil Morampudi <akhil@Akhils-MacBook-Pro.local>
2020-06-03 10:54:15 -0500

    stage

--- mergesort.c
+++ mergesort.c
@@ -15,7 +15,7 @@
 		int R[n2];
 
 		/*Copy data to temp arrays*/
-		for (int i=0; i<n1; ++i) 
+		for (int i=5; i<n1; ++i) 
 			L[i] = arr[l + i]; 
 		for (int j=0; j<n2; ++j) 
 			R[j] = arr[m + 1+ j]; 
